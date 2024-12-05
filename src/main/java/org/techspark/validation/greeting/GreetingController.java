2e5b12fbd56e8d68f894522243a99ba748255ce9
Prasath Basuvaraj <prasath.buddie@gmail.com>
2024-12-05 13:14:53 -0500

    Initial skeleton

---
+++ src/main/java/org/techspark/validation/greeting/GreetingController.java
@@ -0,0 +1,12 @@
+package org.techspark.validation.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting() {
+        return "Greeting from TechSpark";
+    }
+}
---
+++ src/test/java/org/techspark/validation/ApplicationTest.java
@@ -0,0 +1,6 @@
+package org.techspark.validation;
+
+
+class ApplicationTest {
+
+}
